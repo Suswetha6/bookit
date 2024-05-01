a96f88f7cd0975ea08e0a7842eaddffdedf22b4d
Anuj-Wanjari <141702803+Anuj-Wanjari@users.noreply.github.com>
2024-05-01 23:05:01 +0530

    feat : created customer controller & repository (#5)

--- src/main/java/org/abhinavgpt/bookit/controllers/CustomerController.java
+++ src/main/java/org/abhinavgpt/bookit/controllers/CustomerController.java
@@ -1,4 +1,75 @@
 package org.abhinavgpt.bookit.controllers;
 
+import org.abhinavgpt.bookit.exceptions.CustomerNotFoundException;
+import org.abhinavgpt.bookit.modals.Customer;
+import org.abhinavgpt.bookit.services.customer.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/customers")
 public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService){
+        this.customerService = customerService;
+    }
+
+    //create new customer
+    @PostMapping
+    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
+        Customer newCustomer = customerService.createCustomer(customer);
+        return new ResponseEntity<>(newCustomer, HttpStatus.CREATED);
+    }
+
+    //retrieve all customer
+    @GetMapping
+    public ResponseEntity<List<Customer>> getAllCustomers() {
+        List<Customer> customers = customerService.getAllCustomers();
+        return new ResponseEntity<>(customers, HttpStatus.OK);
+    }
+
+    //retrieve a customer by id
+    @GetMapping("/{customerId}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable("customerId") Long customerId) {
+        try{
+            Customer customer = customerService.getCustomerById(customerId);
+            return new ResponseEntity<>(customer, HttpStatus.OK);
+        }catch(CustomerNotFoundException e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    //update a customer
+    @PutMapping("/{customerId}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable("customerId") Long customerId,
+                                                   @RequestBody Customer updatedCustomer) {
+        try{
+            Customer customer = customerService.updateCustomer(customerId, updatedCustomer);
+            return new ResponseEntity<>(customer, HttpStatus.OK);
+        } catch (CustomerNotFoundException e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    //Delete a customer
+    @DeleteMapping("/{customerId}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable("customerId") Long customerId) {
+        try{
+            boolean deleted = customerService.deleteCustomer(customerId);
+            if (deleted) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }else{
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (CustomerNotFoundException e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
