39db0d991b9ad68c5102c7899612d09afb8dece5
lokendra005 <141302553+lokendra005@users.noreply.github.com>
2024-05-01 21:32:23 +0530

    feat: Created Events Controller and Repository (#2)
    * first commit added the event controller
    
    * first commit added the event controller
    updated the EventController
    
    * first commit added the event controller
    updated the EventController
    Created EventRepository and EventServiceImpl
    
    * feat: created events worklow

--- src/main/java/org/abhinavgpt/bookit/controllers/EventController.java
+++ src/main/java/org/abhinavgpt/bookit/controllers/EventController.java
@@ -1,4 +1,63 @@
 package org.abhinavgpt.bookit.controllers;
 
+import org.abhinavgpt.bookit.modals.Event;
+import org.abhinavgpt.bookit.services.event.EventService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
 public class EventController {
+    private EventService eventService;
+
+    // add
+    @PostMapping
+    public ResponseEntity<Event> addEvent(@RequestBody Event event) {
+        Event newEvent = EventService.addEvent(event);
+        return ResponseEntity.ok(newEvent);
+
+    }
+
+    //update
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Event> updateEvent(@PathVariable long id ,@RequestBody Event Updatedevent) {
+        Event event = EventService.updateEvent(Updatedevent);
+        return ResponseEntity.ok(event);
+    }
+
+    //getAll
+
+    @GetMapping()
+    public ResponseEntity<List<Event>> getAllEvents() {
+        List<Event>  event = eventService.getAllEvents();
+        return ResponseEntity.ok(event);
+    }
+
+    // getById
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Event> getEvent(@PathVariable long id) {
+        Event event = EventService.getEvent((int) id);
+
+        return ResponseEntity.ok(event);
+    }
+
+    // deleteById
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Event> deleteEvent(@PathVariable long id) {
+        EventService.deleteEventById((int) id);
+        return new ResponseEntity<>("Event deleted successfully.", HttpStatus.NO_CONTENT);
+    }
+
+    // deleteAll
+//    @DeleteMapping()
+//    public ResponseEntity<Event> deleteAllEvents() {
+//        return null;
+//    }
+
 }
